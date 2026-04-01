7651eec15b7a0229b3435e080a509f1b2a3694dc
Dino Pizek <dinovzd@gmail.com>
2026-04-01 14:52:31 +0200

    Add subscription context setup to resource group creation script

--- rg/resource-group-create.ps1
+++ rg/resource-group-create.ps1
@@ -1,6 +1,19 @@
 # Define the following parameters.
 $ResourceGroup = "rg-gewe-dino-lab-01"
 $Location = "Germany West Central"
+$SubscriptionName = "rev-sub-corporate-workload-dev"
+
+$CriticalityTag = "Low"
+$DomainTag = "Corporate"
+
+# Get the subscription id.
+$Subscription = Get-AzSubscription -SubscriptionName $SubscriptionName
+
+# Select the subscription context.
+Set-AzContext -SubscriptionId $Subscription.Id | Out-Null
 
 #Create a resource group.
-New-AzResourceGroup -Name $ResourceGroup -Location $Location
+#New-AzResourceGroup -Name $ResourceGroup -Location $Location
+
+#Create a resource group with tags.
+New-AzResourceGroup -Name $ResourceGroup -Location $Location -Tag @{Criticality=$CriticalityTag; Domain=$DomainTag}
