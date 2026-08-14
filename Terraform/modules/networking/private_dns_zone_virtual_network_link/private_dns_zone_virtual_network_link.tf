466fcc108cdc08143ac054180c1a347afade7424
dinopizek <dinovzd@gmail.com>
2026-08-14 09:41:54 +0200

    CU

--- Terraform/modules/networking/private_dns_zone_virtual_network_link/private_dns_zone_virtual_network_link.tf
+++ Terraform/modules/networking/private_dns_zone_virtual_network_link/private_dns_zone_virtual_network_link.tf
@@ -2,4 +2,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_virtu
   name                = var.name
   private_dns_zone_id = var.private_dns_zone_id
   virtual_network_id  = var.virtual_network_id
+  resolution_policy   = try(var.settings.resolution_policy, "NxDomainRedirect")
 }
